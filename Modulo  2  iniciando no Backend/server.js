385ef5d1e8bf258e3d95777cdb3d17cb8c5a749f
felipeghidini <lipeghidini18@gmail.com>
2021-11-20 06:39:01 -0300

    Instalando Nunjucks e gerando as rotas das páginas

--- Modulo  2  iniciando no Backend/server.js
+++ Modulo  2  iniciando no Backend/server.js
@@ -1,8 +1,22 @@
 const express = require('express');
+const nunjucks = require('nunjucks');
+
 const server = express();
 
+server.use(express.static('public'));
+
+server.set('view engine', 'html');
+
+nunjucks.configure('views', {
+    express: server
+})
+
 server.get('/', (req, res) => {
-    return res.send('Jesus é o SALVADOR');
+    return res.render('index');
+})
+
+server.get('/classes', (req, res) => {
+    return res.render('classes');
 })
 
 server.listen(5000, () => {
