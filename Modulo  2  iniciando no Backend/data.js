507ca9fe13c79d0f6a5472434d6d7c1ce138226f
felipeghidini <lipeghidini18@gmail.com>
2021-11-21 10:04:00 -0300

    Usando dados dinâmicos

---
+++ Modulo  2  iniciando no Backend/data.js
@@ -0,0 +1,38 @@
+module.exports = [
+    {
+        id: "cZvWDTqTGKg",
+        title: "Quadro de imagens igual ao Pinterest com CSS puro",
+        duration: "14min",
+        price: "Free"
+    },
+    {
+        id: "CITYla5utT4",
+        title: "Campo HTML para inserir tags com CSS e JavaScript",
+        duration: "37min",
+        price: "Free"
+    },
+    {
+        id: "Z-f8868RTE4",
+        title: "TDD com JavaScript",
+        duration: "121min",
+        price: "Free"
+    },
+    {
+        id: "RkLjxfUTeKc",
+        title: "Desmistificando MAP, FILTER e REDUCE",
+        duration: "110min",
+        price: "Free"
+    },
+    {
+        id: "Uptu3NrBFBM",
+        title: "Aplicação com JavaScript puro: Como desenvolver uma pokedex com JS",
+        duration: "32min",
+        price: "Free"
+    },
+    {
+        id: "p34kaxW0WjU",
+        title: "Crud em 12 minutos",
+        duration: "13min",
+        price: "Free"
+    },
+]
--- Modulo  2  iniciando no Backend/server.js
+++ Modulo  2  iniciando no Backend/server.js
@@ -2,21 +2,34 @@ const express = require('express');
 const nunjucks = require('nunjucks');
 
 const server = express();
+const videos = require("./data");
 
 server.use(express.static('public'));
 
 server.set('view engine', 'njk');
 
 nunjucks.configure('views', {
-    express: server
+    express: server,
+    autoescape: false
 })
 
 server.get('/', (req, res) => {
-    return res.render('sobre');
+    const data = {
+        avatar_url: "https://avatars.githubusercontent.com/u/49081674?s=400&u=8ae5c833ff4fe50cae5a2209e7f5fed13f671619&v=4",
+        name: "Felipe Ghidini",
+        role: "Aluno Rocketseat",
+        description: "Programador focado em Desenvolvimento Web, que procura um maior aperfeiçoamento a cada dia",
+        links: [
+            { name: "Github", url: 'https://github.com/felipeghidini'},
+            { name: "Linkedin", url: "https://www.linkedin.com/in/felipe-ghidini-471a3510a/"}
+        ]
+    }
+
+    return res.render('sobre', { sobre: data });
 })
 
 server.get('/classes', (req, res) => {
-    return res.render('classes');
+    return res.render('classes', { items: videos });
 })
 
 server.listen(5000, () => {
