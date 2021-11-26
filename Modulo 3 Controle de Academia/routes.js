ea59703db9149d32b52f2edcd735c14a98163628
felipeghidini <lipeghidini18@gmail.com>
2021-11-26 06:52:50 -0300

    Separando funções das rotas, usando Node fs

--- Modulo 3 Controle de Academia/routes.js
+++ Modulo 3 Controle de Academia/routes.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const routes = express.Router();
+const instructors = require('./instructors');
 
 routes.get('/', (req, res) => {
     return res.redirect('/instructors');
@@ -13,17 +14,7 @@ routes.get('/instructors/create', (req, res) => {
     return res.render('instructors/create');
 })
 
-routes.post('/instructors', (req, res) => {
-    const keys = Object.keys(req.body);
-
-    for (key of keys) {
-        if (req.body[key] == '') {
-            return res.send('Please, fill all fields');
-        }
-    }
-    
-    return res.send(req.body);
-})
+routes.post('/instructors', instructors.post)
 
 routes.get('/members', (req, res) => {
     return res.send('members');
