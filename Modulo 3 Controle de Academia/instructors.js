28c5cd4031b457d63521cd2cd3eec5254c56e518
felipeghidini <lipeghidini18@gmail.com>
2021-11-26 07:08:29 -0300

    Trabalhando com dados em JSON e datas no JS

--- Modulo 3 Controle de Academia/instructors.js
+++ Modulo 3 Controle de Academia/instructors.js
@@ -1,4 +1,5 @@
 const fs = require('fs');
+const data = require('./data.json');
 
 // create
 exports.post = (req, res) => {
@@ -10,7 +11,12 @@ exports.post = (req, res) => {
         }
     }
 
-    fs.writeFile("data.json", JSON.stringify(req.body), (err) => {
+    req.body.birth = Date.parse(req.body.birth);
+    req.body.created_at = Date.now();
+
+    data.instructors.push(req.body); 
+
+    fs.writeFile("data.json", JSON.stringify(data, null, 2), (err) => {
         if (err) {
             return res.send('Write file error!');
         }
