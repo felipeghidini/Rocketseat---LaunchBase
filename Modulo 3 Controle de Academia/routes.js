4ece0864a6954fac6eeadc8523c09252ea044313
felipeghidini <lipeghidini18@gmail.com>
2021-11-25 22:32:58 -0300

    Estilizando o formulário, recebendo os dados do front e validando dados no back

--- Modulo 3 Controle de Academia/routes.js
+++ Modulo 3 Controle de Academia/routes.js
@@ -14,7 +14,15 @@ routes.get('/instructors/create', (req, res) => {
 })
 
 routes.post('/instructors', (req, res) => {
-    return res.send('recebido')
+    const keys = Object.keys(req.body);
+
+    for (key of keys) {
+        if (req.body[key] == '') {
+            return res.send('Please, fill all fields');
+        }
+    }
+    
+    return res.send(req.body);
 })
 
 routes.get('/members', (req, res) => {
