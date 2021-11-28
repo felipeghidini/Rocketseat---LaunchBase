5694cab358f23f5a56721bf580804054ce0638b6
felipeghidini <lipeghidini18@gmail.com>
2021-11-28 09:11:50 -0300

    Formatando datas para o formulário

--- Modulo 3 Controle de Academia/instructors.js
+++ Modulo 3 Controle de Academia/instructors.js
@@ -1,6 +1,6 @@
 const fs = require('fs');
 const data = require('./data.json');
-const { age } = require('./utils');
+const { age, date } = require('./utils');
 
 exports.show = (req, res) => {
     const { id } = req.params;
@@ -59,3 +59,22 @@ exports.post = (req, res) => {
     // return res.send(req.body);
 }
 
+exports.edit = (req, res) => {
+    const { id } = req.params;
+
+    const foundInstructor = data.instructors.find(instructor => {
+        return instructor.id == id;
+    })
+
+    if (!foundInstructor) {
+        return res.send('Instructor not found');
+    }
+
+    const instructor = {
+        ...foundInstructor,
+        birth: date(foundInstructor.birth)
+    }
+
+
+    return res.render('instructors/edit', { instructor });
+}
--- Modulo 3 Controle de Academia/utils.js
+++ Modulo 3 Controle de Academia/utils.js
@@ -14,22 +14,13 @@ module.exports = {
         }
 
         return age
+    },
+    date: function (timestamp) {
+        const date = new Date(timestamp);
+        const year = date.getUTCFullYear();
+        const month = `0${date.getUTCMonth() + 1}`.slice(-2);
+        const day = `0${date.getUTCDate()}`.slice(-2);
+
+        return (`${year}-${month}-${day}`);
     }
-}
-
-function age(timestamp) {
-    const today = new Date();
-    const birthDate = new Date(timestamp);
-
-    //2021 - 1989 = 32
-    let age = today.getFullYear() - birthDate.getFullYear();
-    const month = today.getMonth() - birthDate.getMonth();
-
-    // 11 - 12 = -1
-    // 11 - 11 = 0
-    if (month < 0 || month == 0 && today.getDate() < birthDate.getDate()) {
-        age = age - 1
-    }
-
-    return age
 }
