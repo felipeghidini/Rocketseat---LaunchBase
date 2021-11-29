9f887e42e1adc2439f855d801a687982c22ac041
felipeghidini <lipeghidini18@gmail.com>
2021-11-29 16:30:51 -0300

    Incluído os métodos de edição e delete

--- Modulo 3 Controle de Academia/instructors.js
+++ Modulo 3 Controle de Academia/instructors.js
@@ -78,3 +78,54 @@ exports.edit = (req, res) => {
 
     return res.render('instructors/edit', { instructor });
 }
+
+exports.put = (req, res) => {
+    const { id } = req.body;
+    let index = 0;
+
+    const foundInstructor = data.instructors.find((instructor, foundIndex) => {
+        if (id == instructor.id) {
+            index = foundIndex;
+            return true;
+        }
+    })
+
+    if (!foundInstructor) {
+        return res.send('Instructor not found');
+    }
+
+    const instructor = {
+        ...foundInstructor,
+        ...req.body,
+        birth: Date.parse(req.body.birth)
+    }
+
+    data.instructors[index] = instructor;
+
+    fs.writeFile('data.json', JSON.stringify(data, null, 2), (err) => {
+        if (err) {
+            return res.send('Write error!');
+        }
+
+        return res.redirect(`/instructors/${id}`);
+    })
+
+
+}
+
+exports.delete = (req, res) => {
+    const { id } = req.body;
+
+    const filteredInstructors = data.instructors.filter(instructor => {
+        return instructor.id !=id;
+    })
+
+    data.instructors = filteredInstructors;
+
+    fs.writeFile('data.json', JSON.stringify(data, null, 2), err => {
+        if (err) {
+            res.send('Erro!');
+        }
+        return res.redirect('/instructors');
+    })
+}
